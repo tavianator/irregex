346711c2777f957805f7b8788c637976b005f831
Tavian Barnes <tavianator@tavianator.com>
2023-03-03 10:33:25 -0500

    Implement empty regexes

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,17 @@
 //! A simple regular expression engine.
 
+use nom::branch::alt;
+use nom::character::complete::char;
+use nom::combinator::{all_consuming, success};
+use nom::error::Error as NomError;
+use nom::sequence::delimited;
+use nom::{Finish, IResult, Parser};
+
 /// A regular expression.
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum Regex {
+    /// Matches the empty string.
+    Empty,
 }
 
 /// A regular expression matcher.
@@ -23,22 +32,124 @@ pub trait Matcher {
     }
 }
 
+impl<T: Matcher + ?Sized> Matcher for Box<T> {
+    fn start(&mut self) -> bool {
+        self.as_mut().start()
+    }
+
+    fn push(&mut self, c: char) -> bool {
+        self.as_mut().push(c)
+    }
+}
+
 /// An error parsing a regular expression.
-pub type ParseError<'a> = &'a str;
+pub type ParseError<'a> = NomError<&'a str>;
+
+/// Parser implementation.
+fn regex(pattern: &str) -> IResult<&str, Regex> {
+    // Empty : ``
+    let empty = success(Regex::Empty);
+
+    // Group : `(` Regex `)`
+    let group = delimited(char('('), regex, char(')'));
+
+    // Regex : Group | Empty
+    alt((group, empty))
+        .parse(pattern)
+}
 
 impl Regex {
     /// Parse a regular expression.
     pub fn parse(pattern: &str) -> Result<Self, ParseError<'_>> {
-        Err(pattern)
+        all_consuming(regex)
+            .parse(pattern)
+            .finish()
+            .map(|(_, r)| r)
     }
 
     /// Compile a regular expression.
     pub fn matcher(&self) -> Box<dyn Matcher> {
-        todo!()
+        match self {
+            Self::Empty => Box::new(
+                Empty::default()
+            ),
+        }
     }
 
     /// Test if a string matches.
     pub fn matches(&self, text: &str) -> bool {
         self.matcher().matches(text)
+    }
+}
+
+/// Matcher for Regex::Empty.
+#[derive(Debug, Default)]
+pub struct Empty {
+    matched: bool,
+}
+
+impl Matcher for Empty {
+    fn start(&mut self) -> bool {
+        self.matched = true;
+        true
+    }
+
+    fn push(&mut self, _: char) -> bool {
+        self.matched = false;
+        false
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    /// Parse a regex or panic.
+    fn parse(pattern: &str) -> Regex {
+        Regex::parse(pattern)
+            .expect(&format!("Pattern '{pattern}' should parse successfully"))
+    }
+
+    /// Check the result of a successful parse.
+    fn assert_parse(pattern: &str, regex: Regex) {
+        assert_eq!(parse(pattern), regex, "'{pattern}'");
+    }
+
+    /// Check that a parse failed.
+    fn assert_parse_err(pattern: &str) {
+        Regex::parse(pattern)
+            .expect_err(&format!("Pattern '{pattern}' should fail to parse"));
+    }
+
+    #[test]
+    fn parsing() {
+        assert_parse(r"", Regex::Empty);
+        assert_parse(r"()", Regex::Empty);
+        assert_parse(r"(())", Regex::Empty);
+
+        assert_parse_err(r"(");
+        assert_parse_err(r")");
+    }
+
+    /// Check whether a regex matches a set of strings.
+    fn assert_matches(pattern: &str, matches: &[&str], non_matches: &[&str]) {
+        let regex = parse(pattern);
+
+        for text in matches {
+            assert!(regex.matches(text), "'{pattern}' should match '{text}'");
+        }
+
+        for text in non_matches {
+            assert!(!regex.matches(text), "'{pattern}' should not match '{text}'");
+        }
+    }
+
+    #[test]
+    fn matching() {
+        assert_matches(
+            r"",
+            &[""],
+            &["a", "b", "ab"],
+        );
     }
 }
